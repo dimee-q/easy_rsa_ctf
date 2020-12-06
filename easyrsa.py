964e4699cf79fd12b5f4fab67635fdb94aff2f00
dimee-q <75311233+dimee-q@users.noreply.github.com>
2020-12-06 13:40:58 +0300

    Update easyrsa.py

--- easyrsa.py
+++ easyrsa.py
@@ -1,6 +1,8 @@
 from Crypto.Util.number import inverse
 
-print('On the factordb.com you can get p and q using n.\nEnter only decimal numbers.')
+
+print("On the factordb.com you can get p and q using n.\n"
+      "Enter only decimal numbers.")
 
 n = int(input('Enter n: '))
 e = int(input('Enter e: '))
@@ -8,8 +10,8 @@ c = int(input('Enter c: '))
 p = int(input('Enter p: '))
 q = int(input('Enter q: '))
 
-phi = (p-1)*(q-1)
-d = inverse(e,phi)
-m = pow(c,d,n)
+phi = (p - 1) * (q - 1)
+d = inverse(e, phi)
+m = pow(c, d, n)
 res = bytearray.fromhex(str(hex(m)).split('x')[-1]).decode()
 print('Flag:', res)
